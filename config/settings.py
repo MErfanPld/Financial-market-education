e2704efdf819c6816fb2ca83c99dacb4319386e3
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-12-09 09:36:52 -0500

    Add Users and Account App

--- config/settings.py
+++ config/settings.py
@@ -85,22 +85,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'financial_market_db_1404',
-        'USER': 'financial_market_1404',
-        'PASSWORD': 'erfan1070',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'financial_market_db_1404',
+#         'USER': 'financial_market_1404',
+#         'PASSWORD': 'erfan1070',
+#     }
+# }
 
 
 # Password validation
