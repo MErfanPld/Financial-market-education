381e57da1c4959c6004b704be9dadf388530aaa7
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-12-09 09:46:05 -0500

    Add Users and Account App

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!6z)5zl75nkp(#3g7yh1_kh5y(9api#%it4c^_6q)7!@db5*q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
